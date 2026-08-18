cd2583c4f41c21a50c7604ccd81f94a9c11621ae
Juanluccianos <contabilidad@luccianos.com.ar>
2026-08-18 14:33:07 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker · Viáticos Lucciano's
    Recordá: subir SIEMPRE index.html + sw.js juntos y bumpear la versión. */
-const CACHE = "viaticos-v07";
+const CACHE = "viaticos-v08";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", e => {
